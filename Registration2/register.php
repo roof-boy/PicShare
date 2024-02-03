77c73d7a64aa50235cf30a6eb87ddbbe56fb6fbf
OfflineWork <sovaelias@gmail.com>
2024-02-03 22:22:33 +0200

    Added Backend check for user and email

--- Registration2/register.php
+++ Registration2/register.php
@@ -22,25 +22,47 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+# Check if username already exists
+
+$query_check_username = "SELECT * FROM users WHERE username = ?";
+$stmt_check_username = mysqli_prepare($conn, $query_check_username);
+mysqli_stmt_bind_param($stmt_check_username, "s", $formUser);
+mysqli_stmt_execute($stmt_check_username);
+mysqli_stmt_store_result($stmt_check_username);
+if(mysqli_stmt_num_rows($stmt_check_username) > 0) {
+    header("Location: register.html?registration=failedUser");
+    exit();
+}
+
+# Check if email already exists
+
+$query_check_email = "SELECT * FROM users WHERE email = ?";
+$stmt_check_email = mysqli_prepare($conn, $query_check_email);
+mysqli_stmt_bind_param($stmt_check_email, "s", $formMail);
+mysqli_stmt_execute($stmt_check_email);
+mysqli_stmt_store_result($stmt_check_email);
+if(mysqli_stmt_num_rows($stmt_check_email) > 0) {
+    header("Location: register.html?registration=failedEmail");
+    exit();
+}
+
 # Insert record
 
-$query = "INSERT INTO users (username, passw, email) VALUES (?, ?, ?)";
-$stmt = mysqli_prepare($conn, $query);
-mysqli_stmt_bind_param($stmt, "sss", $formUser, $hashedPassword, $formMail);
-// Bind other parameters if needed
+$query_insert = "INSERT INTO users (username, passw, email) VALUES (?, ?, ?)";
+$stmt_insert = mysqli_prepare($conn, $query_insert);
+mysqli_stmt_bind_param($stmt_insert, "sss", $formUser, $hashedPassword, $formMail);
 
 // Step 4: Execute the Query
-if(mysqli_stmt_execute($stmt)) {
-    $success = true;
-    if ($success) {
-        header("Location: register.html?registration=true"); // Redirect with success parameter
-        exit();
-    }
+if(mysqli_stmt_execute($stmt_insert)) {
+    header("Location: register.html?registration=true"); // Redirect with success parameter
+    exit();
 } else {
     echo "Error inserting data: " . mysqli_error($conn);
 }
 
-// Close statement and connection
-mysqli_stmt_close($stmt);
+// Close statements and connection
+mysqli_stmt_close($stmt_insert);
+mysqli_stmt_close($stmt_check_username);
+mysqli_stmt_close($stmt_check_email);
 mysqli_close($conn);
-
+?>
