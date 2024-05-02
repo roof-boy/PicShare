31daa3405669c30130fb55ebec71132940cd0dfb
roof-boy <79036070+roof-boy@users.noreply.github.com>
2024-05-02 21:43:54 +0300

    updated incorrect url in config.php

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ $database = "picshare";
 
 # BASE PATH/URL CONFIGURATION
 
-define('BASE_URL', 'http://localhost/instacopy/'); # BASE URL
+define('BASE_URL', 'http://localhost/PicShare/'); # BASE URL
 
 define('BASE_PATH', dirname(__FILE__));
 
